1b6dab271a311b2d402641efd57a4b079c08e7c7
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-21 00:35:30 +0500

    apollo provider was added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
+import { ApolloProvider } from '@apollo/client';
+import { client } from './apollo/apollo_init';
+
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(<App />);
+
+root.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+);
