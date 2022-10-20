74b1f89d38708446ee5304865e1a82ebfc98c0b7
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-20 15:34:35 +0500

    fields were added with formik and mui

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
+import { Box, TextField } from '@mui/material';
+import { useFormik } from 'formik';
 
-export const Registration:React.FC = () => {
+export const Registration: React.FC = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            password: '',
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
     return (
-        <div>
-        </div>
+        <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+            <form onSubmit={formik.handleSubmit}>
+                <TextField
+                    sx={{ width: '300px', height: '200px' }}
+                    label="Outlined"
+                    variant="outlined"
+                    name="name"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.name}
+                />
+                <TextField
+                    sx={{ width: '300px', height: '200px' }}
+                    label="Outlined"
+                    variant="outlined"
+                    name="email"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+                <TextField
+                    sx={{ width: '300px', height: '200px' }}
+                    label="Outlined"
+                    variant="outlined"
+                    name="password"
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                />
+                <button type='submit'>submit.</button>
+            </form>
+        </Box>
     );
-}
+};
 
 export default Registration;
