13c938a8e9da8d84bc9b9d4fd782904dd2a07296
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-21 19:47:07 +0500

    mutation was rewritten to normalized form

--- src/apollo/mutations/Login.ts
+++ src/apollo/mutations/Login.ts
@@ -1,16 +1,9 @@
 import { gql } from '@apollo/client';
 
-interface loginProps {
-    email: string,
-    password: string,
-}
-
-export const LOGIN = ({email, password}: loginProps) => {
-    return gql`
-        mutation {
-            login(input: { email: "${email}", password: "${password}" }) {
-                id
-            }
+export const LOGIN = gql`
+    mutation Login($input: LoginInput!) {
+        login(input: $input) {
+            id
         }
-    `;
-};
+    }
+`;
