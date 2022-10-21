6e7f0f6adc682b3625dfadea58ab343be627a05a
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-22 00:58:59 +0500

    username is parsed from localstorage

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react'
-import { Outlet } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
 import { Header } from '../../components/Header';
+import { UserMap } from '../../shared/constants/localStorageMap';
 
 export const Main: React.FC = () => {
     const [username, setUsername] = useState('');
-  return (
-    <div>
-        <Header username={username}/>
-        <Outlet context={{setUsername}}/>
-    </div>
-  )
-}
+
+    useEffect(() => {
+        const localUsername = localStorage.getItem(UserMap.USERNAME);
+        if (localUsername) {
+            setUsername(localUsername);
+        }
+    }, []);
+
+    return (
+        <div>
+            <Header username={username} setUsername={setUsername} />
+            <Outlet context={{ setUsername }} />
+        </div>
+    );
+};
