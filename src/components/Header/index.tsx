d51a48e4f111f0c76cb4873cfb2e736591bab08b
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-22 00:57:32 +0500

    logout option was added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,34 @@
 import { Search } from '@mui/icons-material';
-import { IconButton, Switch, TextField } from '@mui/material';
-import React from 'react';
+import { IconButton, Switch, TextField, Tooltip } from '@mui/material';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import style from './Header.module.scss';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { useMutation } from '@apollo/client';
+import { LOGOUT } from '../../apollo/mutations/Logout';
+import { UserMap } from '../../shared/constants/localStorageMap';
 
 interface HeaderProps {
     username: string;
+    setUsername: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export const Header: React.FC<HeaderProps> = ({username}) => {
-    
+export const Header: React.FC<HeaderProps> = ({ username, setUsername }) => {
+    const [logout, { error, data }] = useMutation(LOGOUT);
+
+    useEffect(() => {
+        if (error) {
+            console.log(error);
+        }
+    }, [error]);
+
+    useEffect(() => {
+        if (data) {
+            localStorage.removeItem(UserMap.USERNAME);
+            setUsername('');
+        }
+    }, [data]);
+
     return (
         <header className={style.header}>
             <Link to="/">colle.</Link>
@@ -23,7 +42,14 @@ export const Header: React.FC<HeaderProps> = ({username}) => {
                 <Search />
             </IconButton>
             {username ? (
-                <div>{username}</div>
+                <div>
+                    {username}
+                    <Tooltip title="Logout">
+                        <IconButton onClick={() => logout()}>
+                            <LogoutIcon />
+                        </IconButton>
+                    </Tooltip>
+                </div>
             ) : (
                 <>
                     <Link to="/login">Login</Link>
