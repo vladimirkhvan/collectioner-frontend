60b1732b06148ebaa9cdd202303ea87deec9aa95
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-21 19:48:30 +0500

    app directory was substituted with router

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
 
 import { ApolloProvider } from '@apollo/client';
 import { client } from './apollo/apollo_init';
+
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/router';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
     <ApolloProvider client={client}>
-        <App />
+        <RouterProvider router={router} />
     </ApolloProvider>,
 );
