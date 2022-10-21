014b556426b18f363cb912557b97abeeb74bfd22
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-21 23:47:05 +0500

    links were added / login and registration field is conditionally rendered

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,35 @@
-import { Switch } from '@mui/material';
+import { Search } from '@mui/icons-material';
+import { IconButton, Switch, TextField } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import style from './Header.module.scss';
 
-export const Header: React.FC = () => {
+interface HeaderProps {
+    username: string;
+}
+
+export const Header: React.FC<HeaderProps> = ({username}) => {
+    
     return (
         <header className={style.header}>
-            <span>colle.</span>
+            <Link to="/">colle.</Link>
             <Switch
                 // checked={checked}
                 // onChange={handleChange}
                 inputProps={{ 'aria-label': 'controlled' }}
             />
+            <TextField id="standard-basic" label="Standard" variant="standard" />
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                <Search />
+            </IconButton>
+            {username ? (
+                <div>{username}</div>
+            ) : (
+                <>
+                    <Link to="/login">Login</Link>
+                    <Link to="/registration">Register</Link>
+                </>
+            )}
         </header>
     );
 };
