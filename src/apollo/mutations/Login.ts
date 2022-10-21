d2ec3db256605b7b8fbe51fa53fff98ab514f147
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-21 23:45:39 +0500

    name is returned

--- src/apollo/mutations/Login.ts
+++ src/apollo/mutations/Login.ts
@@ -3,7 +3,8 @@ import { gql } from '@apollo/client';
 export const LOGIN = gql`
     mutation Login($input: LoginInput!) {
         login(input: $input) {
-            id
+            id,
+            name
         }
     }
 `;
