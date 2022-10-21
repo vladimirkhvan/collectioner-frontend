d83560fd32446032bb15d4492b15ef8cee4a1a41
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-21 23:50:25 +0500

    context was added

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from "react-router-dom";
 import { Header } from '../../components/Header';
 
 export const Main: React.FC = () => {
+    const [username, setUsername] = useState('');
   return (
     <div>
-        <Header />
-        <Outlet/>
+        <Header username={username}/>
+        <Outlet context={{setUsername}}/>
     </div>
   )
 }
