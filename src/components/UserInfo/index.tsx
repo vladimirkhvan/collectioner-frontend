212f8e57ddb64ae11dd0c71d8efd65c7d341cae4
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-22 16:32:34 +0500

    user data is fetched

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import { useQuery } from '@apollo/client';
 import { GET_ME } from '../../apollo/queries/GetMe';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 export const UserInfo: React.FC = () => {
     const { loading, error, data } = useQuery(GET_ME);
+    const navigate = useNavigate();
 
     if (loading) {
         return <div>loading...</div>;
@@ -14,7 +17,18 @@ export const UserInfo: React.FC = () => {
         return <div>error occured</div>;
     }
 
-    console.log(data.getMe);
+    if(data.getMe === null){
+        navigate('/');
+        return <div>error occured</div>;
+    } 
 
-    return <div>{data.getMe}</div>;
+    return (
+        <div>
+            <span>{data.getMe.id}. </span>
+            <span>{data.getMe.name}</span>
+            
+            <Button variant="outlined">create collection</Button>
+
+        </div>
+    );
 };
