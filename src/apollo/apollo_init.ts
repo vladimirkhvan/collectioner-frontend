32fcf8ec6beb722cc6e0caf4a59606ee0588929f
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-22 16:21:52 +0500

    credentials include option added

--- src/apollo/apollo_init.ts
+++ src/apollo/apollo_init.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:8800/graphql',
     cache: new InMemoryCache(),
+    uri: 'http://localhost:8800/graphql',
+    credentials: 'include',
 });
