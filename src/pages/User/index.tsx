07d2c1f8c4c6af40b613a79911bd0bbfb960ed75
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-10-27 00:28:14 +0500

    collection form was added

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { CollectionForm } from '../../components/CollectionForm';
 import { UserInfo } from '../../components/UserInfo';
 
 export const User: React.FC = () => {
 
     return <div>
         <UserInfo/>
+        <CollectionForm/>
     </div>;
 };
